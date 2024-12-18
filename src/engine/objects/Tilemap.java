708175ec27124b4f168398b325fa1bf6dd2b7a7d
Qufel <97023524+Qufel@users.noreply.github.com>
2024-12-18 22:56:41 +0100

    Pixel assignment for tile group sprites. Part 1

--- src/engine/objects/Tilemap.java
+++ src/engine/objects/Tilemap.java
@@ -1,11 +1,11 @@
 package engine.objects;
 
 import engine.Engine;
+import engine.graphics.Sprite;
 import engine.graphics.Tileset;
 import engine.physics.Vector2;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,11 +13,10 @@ import java.util.Arrays;
 
 public class Tilemap extends StaticBody {
 
-    //TODO: Load layout [x]
     //TODO: Generating sprite/s [ ]
     //TODO: Generating colliders [ ]
 
-    private final int MAX_TILES = 16;
+    private final int MAX_TILES = 8;
 
     private Tileset tileset;
 
@@ -118,7 +117,102 @@ public class Tilemap extends StaticBody {
 
     @Override
     public void start(Engine engine) {
+        createSprites();
+    }
+
+    private void createSprites() {
+        Sprite[] sprites;
+
+        // Get count of how many sprites to generate
+
+        int countX = Math.floorDiv(this.width, MAX_TILES);
+        int countY = Math.floorDiv(this.height, MAX_TILES);
+
+        if (this.width - countX * MAX_TILES> 0)
+            countX++;
+
+        if (this.height - countY * MAX_TILES> 0)
+            countY++;
+
+        sprites = new Sprite[countX * countY];
+
+        for (int y = 0; y < countY; y++) {
+            for (int x = 0; x < countX; x++) {
+
+                // Determine width and height of each sprite
+                int width = MAX_TILES;
+                int height = MAX_TILES;
+
+                if (width * (x + 1) > this.width)
+                    width = this.width - MAX_TILES * x;
+
+                if (height * (y + 1) > this.height)
+                    height = this.height - MAX_TILES * y;
+
+                // Create sprite
+                int[] pixels = createPixelArray(x, y, width, height);
+                sprites[y * (countX) + x] = new Sprite(pixels, width, height, false, false);
+
+                for (Sprite sprite : sprites) {
+                    this.addChildren(
+                            new Sprite2D(this, "Sprite_" + x + "_" + y, Vector2.ZERO, sprite)
+                    );
+                }
+
+            }
+        }
+
+    }
+
+    private int[] createPixelArray(int xCoord, int yCoord, int width, int height) {
+        int cellW = tileset.getCellWidth(), cellH = tileset.getCellHeight();
+        int[] pixels = new int[(cellW * width) * (cellH * height)];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+
+                // Get offset values for each tile
+                int offsetX, offsetY;
+
+                if (width < MAX_TILES)
+                    offsetX = this.width - width - 1;
+                else
+                    offsetX = xCoord * width;
+
+                if (height < MAX_TILES)
+                    offsetY = this.height - height - 1;
+                else
+                    offsetY = yCoord * height;
+
+                // Get tile id
+                int tile = getTileIdAt(j + offsetX, i + offsetY);
+
+                int[] tilePixels = new int[cellW * cellH];
+
+                // Get tile pixels
+                if (tile == -1) {
+                    Arrays.fill(tilePixels, 0x00000000);
+                } else {
+                    tilePixels = tileset.getTile(tile);
+                }
+
+                for (int y = 0; y < cellH; y++) {
+                    for (int x = 0; x < cellW; x++) {
+
+                        int index = indexAt(x, y, i, j);
+                        pixels[index] = tilePixels[y * cellW + x];
+                    }
+                }
+
+            }
+        }
 
 
+        return pixels;
+    }
+
+    private int indexAt(int x, int y, int i, int j) {
+        int cellW = tileset.getCellWidth(), cellH = tileset.getCellHeight();
+        return y * (((cellW * width) - ((width - 1) * cellW))) + i * cellW + (y * cellW + x) + j * cellH * (width * cellW);
     }
 }
