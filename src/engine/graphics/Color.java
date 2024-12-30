225f3527582ad67e414e0c3ace76d6e1d69784b0
Qufel <97023524+Qufel@users.noreply.github.com>
2024-12-30 11:15:00 +0100

    Fixed color transparency

--- src/engine/graphics/Color.java
+++ src/engine/graphics/Color.java
@@ -4,15 +4,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Color {
     
-    public static final int RED = 0xff0000;
-    public static final int GREEN = 0x00ff00;
-    public static final int BLUE = 0x0000ff;
-    public static final int BLACK = 0x000000;
-    public static final int WHITE = 0xffffff;
-    public static final int GRAY = 0x808080;
-    public static final int CYAN = 0x00ffff;
-    public static final int MAGENTA = 0xff00ff;
-    public static final int YELLOW = 0xffff00;
+    public static final int RED = 0xffff0000;
+    public static final int GREEN = 0xff00ff00;
+    public static final int BLUE = 0xff0000ff;
+    public static final int BLACK = 0xff000000;
+    public static final int WHITE = 0xffffffff;
+    public static final int GRAY = 0xff808080;
+    public static final int CYAN = 0xff00ffff;
+    public static final int MAGENTA = 0xffff00ff;
+    public static final int YELLOW = 0xffffff00;
 
     public static final int randomize() {
         return ThreadLocalRandom.current().nextInt(0x1000000) | 0xff000000;
